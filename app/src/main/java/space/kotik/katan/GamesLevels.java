4f648f616851f76fb90dc0df0d2ce53ef48c4d0d
myKotik <Lebedeva.kate6518@gmail.com>
2021-03-15 18:02:04 +0200

    111

--- app/src/main/java/space/kotik/katan/GamesLevels.java
+++ app/src/main/java/space/kotik/katan/GamesLevels.java
@@ -1,12 +1,18 @@
 package space.kotik.katan;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.transition.Scene;
 import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.Button;
 import android.widget.ImageButton;
-
-import androidx.appcompat.app.AppCompatActivity;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class GamesLevels extends AppCompatActivity {
 
--- app/src/main/java/space/kotik/katan/LevelOne.java
+++ app/src/main/java/space/kotik/katan/LevelOne.java
@@ -1,41 +1,24 @@
 package space.kotik.katan;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MotionEvent;
+import android.transition.Scene;
 import android.view.View;
-import android.view.animation.AlphaAnimation;
-import android.view.animation.Animation;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.ImageSwitcher;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.ViewSwitcher;
-import android.view.GestureDetector;
-import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Toast;
 
-import static space.kotik.katan.R.id.forward;
 
-public class LevelOne extends AppCompatActivity implements ViewSwitcher.ViewFactory, GestureDetector.OnGestureListener{
+public class LevelOne extends AppCompatActivity {
 
-    //массив картинок для слайда
-    private ImageSwitcher mImageSwitcher;
-    int position = 0;
-    private final int[] mImageIds = {R.drawable.one1, R.drawable.one2,
-            R.drawable.one3, R.drawable.one4,
-            R.drawable.one5,R.drawable.one6, R.drawable.one7,R.drawable.one8,
-            R.drawable.one9,R.drawable.one10, R.drawable.one11,R.drawable.one12,
-            R.drawable.one13,R.drawable.one14, R.drawable.one15,R.drawable.one16,
-            R.drawable.one17,R.drawable.one18, R.drawable.one19,R.drawable.one20};
-    private GestureDetector mGestureDetector;
-    private static final int SWIPE_MIN_DISTANCE = 120;
-    private static final int SWIPE_MAX_OFF_PATH = 250;
-    private static final int SWIPE_THRESHOLD_VELOCITY = 100;
 
     TextView text;
-    private MotionEvent e;
 
     @SuppressLint("SetJavaScriptEnabled")
     @Override
@@ -43,21 +26,6 @@ public class LevelOne extends AppCompatActivity implements ViewSwitcher.ViewFact
         super.onCreate(savedInstanceState);
         setContentView(R.layout.universal_level);
 
-        //анимация смены картиок
-        mImageSwitcher = (ImageSwitcher)findViewById(R.id.ImageSwitcher);
-        mImageSwitcher.setFactory(this);
-
-        Animation inAnimation = new AlphaAnimation(0, 1);
-        inAnimation.setDuration(2000);
-        Animation outAnimation = new AlphaAnimation(1, 0);
-        outAnimation.setDuration(2000);
-
-        mImageSwitcher.setInAnimation(inAnimation);
-        mImageSwitcher.setOutAnimation(outAnimation);
-
-        mImageSwitcher.setImageResource(mImageIds[0]);
-
-        mGestureDetector = new GestureDetector(this, this);
 
 
         //загружаем текст уровня
@@ -76,101 +44,6 @@ public class LevelOne extends AppCompatActivity implements ViewSwitcher.ViewFact
                 e.printStackTrace();
             }
         });
-    }
-
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.buttonForward:
-                setPositionNext();
-                mImageSwitcher.setImageResource(mImageIds[position]);
-                break;
-            /*case R.id.buttonPrev:
-                setPositionPrev();
-                mImageSwitcher.setImageResource(mImageIds[position]);
-                break;*/
-
-            //default:
-                //break;
-        }
-    }
-
-    //слайд шоу, очень трудно
-    public void setPositionNext() {
-        position++;
-        if (position > mImageIds.length - 1) {
-            position = 0;
-        }
-    }
-
-    public void setPositionPrev() {
-        position--;
-        if (position < 0) {
-            position = mImageIds.length - 1;
-        }
-    }
-
-    @Override
-    public View makeView() {
-        ImageView imageView = new ImageView(this);
-        imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
-        imageView.setLayoutParams(new
-                ImageSwitcher.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
-        imageView.setBackgroundColor(0xFF000000);
-        return imageView;
-    }
-
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        return mGestureDetector.onTouchEvent(event);
-    }
-
-    @Override
-    public boolean onDown(MotionEvent e) {
-        return false;
-    }
-
-    @Override
-    public void onShowPress(MotionEvent e) {
-
-    }
-
-    @Override
-    public boolean onSingleTapUp(MotionEvent e) {
-        return false;
-    }
-
-    @Override
-    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-        return false;
-    }
-
-    @Override
-    public void onLongPress(MotionEvent e) {
-
-    }
-
-    @Override
-    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-        try {
-            if (Math.abs(e1.getY() - e2.getY()) > SWIPE_MAX_OFF_PATH)
-                return false;
-            // справа налево
-            if (e1.getX() - e2.getX() > SWIPE_MIN_DISTANCE
-                    && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
-                setPositionNext();
-                mImageSwitcher.setImageResource(mImageIds[position]);
-            } else if (e2.getX() - e1.getX() > SWIPE_MIN_DISTANCE
-                    && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
-                // слева направо
-                setPositionPrev();
-                mImageSwitcher.setImageResource(mImageIds[position]);
-            }
-        } catch (Exception e) {
-            // nothing
-            return true;
-        }
-        return true;
     }
 
     //системная кнопка назад
